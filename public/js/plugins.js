316b93f1e3ae372bbc21cf6d0a528ebe548d5719
Taufan Aditya <toopay@taufanaditya.com>
2013-04-21 01:29:49 +0700

    bootstrap twitter

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -22,3 +22,14 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
